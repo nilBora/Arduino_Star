dbd0d82b050fd8f62d747a532dd7a921f29ca813
nilBora <brdnlsrg@gmail.com>
2019-02-08 09:47:02 +0200

    Update Start_v1.0.ino

--- Start_v1.0.ino
+++ Start_v1.0.ino
@@ -1,68 +1,88 @@
-#define DATA_PIN 9 //Пин подключен к DS входу 74HC595
-#define LATCH_PIN 10 //Пин подключен к ST_CP входу 74HC595
-#define CLOCK_PIN 11 //Пин подключен к SH_CP входу 74HC595
+
+#include "StarMode.h"
+
+#define BUTTON_PIN 7
 
 unsigned long prevMillis = 0;
+unsigned long prevMillisCount = 0;
 unsigned int interval = 500;
-unsigned int programCounter = 0;
+unsigned int count = 0;         // переменная для подсчета нажатий на кнопку
+unsigned int button_old = 1;    // предидущее значение входа для отлавливания момента нажатия
+
+const unsigned int maxProgramMod = 5;
+
+StarMode mode = StarMode();
 
 void setup()
 {
   Serial.begin(9600);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
   
+  mode.setPins();
   //устанавливаем режим OUTPUT
-  pinMode(LATCH_PIN, OUTPUT);
-  pinMode(CLOCK_PIN, OUTPUT);
-  pinMode(DATA_PIN, OUTPUT);
+
 }
+
+
 
 void loop()
 {
-  switch (5) {
+  unsigned long currentMillis = millis();
+  if (currentMillis - prevMillisCount > 20) {
+    prevMillisCount = currentMillis;
+    doButtonCount();
+  }
+  //Serial.println(count);
+  switch (count) {
     case 1:
       onDisplayOneProgram();
       break;
     case 2:
       onDisplayTwoProgram();
       break;
-     case 3:
+    case 3:
       onDisplayThreeProgram();
       break;
-     case 4:
+    case 4:
       onDisplayFourProgram();
       break;
-     case 5:
+    case 5:
       onDisplayFiveProgram();
       break;
+    default:
+      onDisplayOneProgram();
     }
 }
 
- const byte dataStepOne[3]= {
-    B00010000, //третий регистр
-    B00010001, //второй регистр
-    B10001000 //первый регистр
- };
+void doButtonCount()
+{
+  if ((digitalRead(BUTTON_PIN) == LOW) && (button_old == 1))
+  { 
+    count++;
+    if (count > maxProgramMod) {
+      count = 1;
+    }
+    Serial.println(count);
+  }
+  button_old = digitalRead(BUTTON_PIN);
 
- const byte dataStepTwo[3] = {
-    B00001010, //третий регистр
-    B10101010, //второй регистр
-    B01010101 //первый регистр
- };
+  return true;
+}
 
- const byte dataStepThree[3] = {
-    B00000100, //третий регистр
-    B01000100, //второй регистр
-    B00100010 //первый регистр
- };
+void onDisplayOneProgram()
+{
+  mode.setRegistersUnits();
+}
 
 void onDisplayTwoProgram()
 {
-  setDataInRegisters(dataStepThree);
-  //delay(500);
-  setDataInRegisters(dataStepTwo);
-  //delay(500);
-  setDataInRegisters(dataStepOne);
-  //delay(500);
+  unsigned long currentMillis = millis();
+ 
+  if (currentMillis - prevMillis > interval) {
+    prevMillis = currentMillis;
+    mode.doTwoProgram();
+  }
+  return true;
 }
 
 void onDisplayThreeProgram()
@@ -71,150 +91,26 @@ void onDisplayThreeProgram()
  
   if (currentMillis - prevMillis > interval) {
     prevMillis = currentMillis;
-    onThreeProgram();
+    mode.doThreeProgram();
   }
 
   return true;
 }
-
-void onThreeProgram()
-{
-  byte resultData[3];
-  if (programCounter == 1) {
-    setDataInRegisters(dataStepThree);
-  }
-
-  if (programCounter == 2) {
-    for (int i=0; i<3; i++) {
-       resultData[i] = dataStepThree[i] | dataStepTwo[i];
-    }
-    
-    setDataInRegisters(dataStepTwo);
-  }
-  
-  if (programCounter == 3) {
-    for (int i=0; i<3; i++) {
-       resultData[i] = dataStepOne[i] | dataStepTwo[i] | dataStepThree[i];
-    }
-    setDataInRegisters(dataStepOne);
-  }
-  
-  if (programCounter == 4) {
-    setRegistersNull();
-    programCounter = 0;
-  }
-  
-  programCounter++;
-
-  return true;
-}
-
-
 
 void onDisplayFourProgram()
 {
   unsigned long currentMillis = millis();
- 
+  Serial.println(currentMillis - prevMillis);
   if (currentMillis - prevMillis > interval) {
-    prevMillis = currentMillis;
-    doFourProgram();
-  }
-
-  return true;
-}
-
-void doFourProgram()
-{
-  byte resultData[3];
-  if (programCounter == 1) {
-    setDataInRegisters(dataStepOne);
-  }
-  if (programCounter == 2) {
-     for (int i=0; i<3; i++) {
-       resultData[i] = dataStepOne[i] | dataStepTwo[i];
-    }
-    setDataInRegisters(resultData);
-  }
-
-  if (programCounter == 3) {
-    for (int i=0; i<3; i++) {
-      resultData[i] = dataStepOne[i] | dataStepTwo[i] | dataStepThree[i];
-    }
     
-    setDataInRegisters(resultData);
+    prevMillis = currentMillis;
+    mode.doFourProgram();
   }
-
-  if (programCounter == 4) {
-    setRegistersNull();
-    programCounter = 0;
-  }
-  
-  programCounter++;
 
   return true;
 }
 
 void onDisplayFiveProgram()
 {
-  byte byteToSend = 0; //Создаем пустой байт B00000000
-  for (int bitPos = 0; bitPos < 8; bitPos++) { // В переменной хранится позиция изменяемого бита
-    byteToSend = 0; // Обнуляем байт при каждом проходе
-    bitWrite(byteToSend, bitPos, HIGH); // При bitPos=0 получим B00000001, при bitPos=1 - B00000010, при bitPos=2 - B00000100 и т.д.
-
-    digitalWrite(LATCH_PIN, LOW);
-    shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, byteToSend); // Инвертируем сигнал при помощи MSBFIRST, грузим с первого бита
-    digitalWrite(LATCH_PIN, HIGH);
-    delay(50);
-  }
-}
-
-void setDataInRegisters(byte *data)
-{
-  digitalWrite(LATCH_PIN, LOW);
-  for (int i=0; i<3; i++) {
-    shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, data[i]);
-  }
-  digitalWrite(LATCH_PIN, HIGH);
-  
-  return true;
-}
-
-void setRegistersNull()
-{
-  digitalWrite(LATCH_PIN, LOW);  
-  shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, 0b00000000);
-  shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, 0b00000000);
-  shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, 0b00000000);
-  digitalWrite(LATCH_PIN, HIGH);
-  return true;
-}
-
-/* Эта функция сдвигает биты влево на одну позицию, перемещая старший бит
-на место младшего. Другими словами, она "вращает" биты по кругу.
-Например, 11110000 превращается в 11100001.
-*/
-void rotateLeft(uint8_t &bits)
-{
-  uint8_t high_bit = bits & (1 << 7) ? 1 : 0;
-  bits = (bits << 1) | high_bit;
-}
-void rotateRight(uint8_t &bits)
-{
-  uint8_t low_bit = bits & 1 ? (1 << 7) : 0;
-  bits = (bits >> 1) | low_bit;
-}
-int previousMillis = 0;
-
-void onDisplayOneProgram()
-{
-  setRegistersUnits();
-}
-
-void setRegistersUnits()
-{
-  digitalWrite(LATCH_PIN, LOW);                        // устанавливаем синхронизацию "защелки" на LOW
-  shiftOut(DATA_PIN, CLOCK_PIN, LSBFIRST, 0b11111111);   // передаем последовательно на dataPin
-  shiftOut(DATA_PIN, CLOCK_PIN, LSBFIRST, 0b11111111);
-  shiftOut(DATA_PIN, CLOCK_PIN, LSBFIRST, 0b11111111);
-  digitalWrite(LATCH_PIN, HIGH);
+  mode.doFiveProgram();
 }
